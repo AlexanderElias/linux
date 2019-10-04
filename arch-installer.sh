f9dac19f0418c53c9c3f7a33e952cdbf7d42340b
Alexander Elias <alex.steven.elias@gmail.com>
2019-10-03 21:37:53 -0700

    fix: indents

--- arch-installer.sh
+++ arch-installer.sh
@@ -54,11 +54,11 @@ DISKS_LENGTH="${#DISKS[*]}"
 # hostname
 answer=""
 while true; do
-  echo ""
-  echo -ne "Hostname: "
-  read answer
-  [ "$answer" != "" ] && break
-  echo "Hostname is required. Try again."
+    echo ""
+    echo -ne "Hostname: "
+    read answer
+    [ "$answer" != "" ] && break
+    echo "Hostname is required. Try again."
 done
 HOSTNAME="$answer"
 
@@ -66,24 +66,24 @@ HOSTNAME="$answer"
 answer=""
 confirm=""
 while true; do
-  echo ""
-  read -s -p "Root Password: " answer
-  echo
-  read -s -p "Root Password (confirm): " confirm
-  echo
-  [ "$answer" = "$confirm" ] && [ "$answer" != "" ] && break
-  echo "Root passwords are empty or do not match. Try again."
+    echo ""
+    read -s -p "Root Password: " answer
+    echo
+    read -s -p "Root Password (confirm): " confirm
+    echo
+    [ "$answer" = "$confirm" ] && [ "$answer" != "" ] && break
+    echo "Root passwords are empty or do not match. Try again."
 done
 ROOT_PASSWORD="$answer"
 
 # username
 answer=""
 while true; do
-  echo
-  echo -ne "Username: "
-  read  answer
-  [ "$answer" != "" ] && break
-  echo "Username is required. Try again."
+    echo
+    echo -ne "Username: "
+    read  answer
+    [ "$answer" != "" ] && break
+    echo "Username is required. Try again."
 done
 USERNAME="$answer"
 
@@ -91,41 +91,41 @@ USERNAME="$answer"
 answer=""
 confirm=""
 while true; do
-  echo
-  read -s -p "User Password: " answer
-  echo
-  read -s -p "User Password (confirm): " confirm
-  echo
-  [ "$answer" = "$confirm" ] && [ "$answer" != "" ] && break
-  echo "User passwords are empty or do not match. Try again."
+    echo
+    read -s -p "User Password: " answer
+    echo
+    read -s -p "User Password (confirm): " confirm
+    echo
+    [ "$answer" = "$confirm" ] && [ "$answer" != "" ] && break
+    echo "User passwords are empty or do not match. Try again."
 done
 USER_PASSWORD="$answer"
 
 # disk
 answer=""
 while true; do
-  echo
-  for (( i=0; i < $DISKS_LENGTH; i++ )) do
-    [ ${i} != 0 ] && echo "${i}) ${DISKS[i]}"
-  done
-  echo -ne "Type the disk number you would like to partition: "
-  read answer
-  [ "${DISKS[answer]}" != "" ] && break
-  echo
-  echo "Disk is required. Try again."
+    echo
+    for (( i=0; i < $DISKS_LENGTH; i++ )) do
+        [ ${i} != 0 ] && echo "${i}) ${DISKS[i]}"
+    done
+    echo -ne "Type the disk number you would like to partition: "
+    read answer
+    [ "${DISKS[answer]}" != "" ] && break
+    echo
+    echo "Disk is required. Try again."
 done
 DISK="${DISKS[answer]}"
 
 # --------------------------------------------------------------------------------
-# Partition 
+# Partition
 # --------------------------------------------------------------------------------
 
 parted -s $DISK \
-  mklabel gpt \
-  mkpart primary fat32 1MiB 512MiB \
-  set 1 boot on \
-  set 1 esp on \
-  mkpart primary ext4 512MiB 100%
+    mklabel gpt \
+    mkpart primary fat32 1MiB 512MiB \
+    set 1 boot on \
+    set 1 esp on \
+    mkpart primary ext4 512MiB 100%
 
 # format
 mkfs.fat -F32 "${DISK}1"
@@ -145,8 +145,8 @@ echo
 echo "Press enter to edit /etc/pacman.d/mirrorlist."
 echo -ne "[e]dit [s]kip: "
 read answer
-if [[ "$answer" != "s" ]]; then 
-  "$EDITOR" /etc/pacman.d/mirrorlist;
+if [[ "$answer" != "s" ]]; then
+    "$EDITOR" /etc/pacman.d/mirrorlist;
 fi
 
 # packages
@@ -157,50 +157,50 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # timezone
 arch-chroot /mnt sh -c "
-  ln -sf '/usr/share/zoneinfo/$TIME_ZONE' /etc/localtime
-  hwclock --systohc
+    ln -sf '/usr/share/zoneinfo/$TIME_ZONE' /etc/localtime
+    hwclock --systohc
 "
 
 # locale
 arch-chroot /mnt sh -c "
-  echo '$LOCALES' >> /etc/locale.gen
-  echo 'LANG=$LANG' > /etc/locale.conf
-  locale-gen
+    echo '$LOCALES' >> /etc/locale.gen
+    echo 'LANG=$LANG' > /etc/locale.conf
+    locale-gen
 "
 
 # hostname
 arch-chroot /mnt sh -c "
-  echo '$HOSTNAME' > /etc/hostname
-  echo '127.0.0.1 localhost' >> /etc/hosts
-  echo '::1 localhost' >> /etc/hosts
-  echo '127.0.1.1 $HOSTNAME.localdomain $HOSTNAME' >> /etc/hosts
+    echo '$HOSTNAME' > /etc/hostname
+    echo '127.0.0.1 localhost' >> /etc/hosts
+    echo '::1 localhost' >> /etc/hosts
+    echo '127.0.1.1 $HOSTNAME.localdomain $HOSTNAME' >> /etc/hosts
 "
 
 # root password
 arch-chroot /mnt sh -c "
-  (echo '$ROOT_PASSWORD'; echo '$ROOT_PASSWORD') | passwd
+    (echo '$ROOT_PASSWORD'; echo '$ROOT_PASSWORD') | passwd
 "
 
 # user
 arch-chroot /mnt sh -c "
-  useradd -Nm -g users -G wheel,sys,audio,input,video,network,rfkill '$USERNAME'
-  (echo '$USER_PASSWORD'; echo '$USER_PASSWORD') | passwd '$USERNAME'
+    useradd -Nm -g users -G wheel,sys,audio,input,video,network,rfkill '$USERNAME'
+    (echo '$USER_PASSWORD'; echo '$USER_PASSWORD') | passwd '$USERNAME'
 "
 
 # sudo
 arch-chroot /mnt sh -c "
-  echo '%wheel ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+    echo '%wheel ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 "
 
 # --------------------------------------------------------------------------------
-# Bootloader 
+# Bootloader
 # --------------------------------------------------------------------------------
 
 # grub
 arch-chroot /mnt sh -c "
-  pacman -S --needed --noconfirm grub
-  grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-  grub-mkconfig -o /boot/grub/grub.cfg
+    pacman -S --needed --noconfirm grub
+    grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+    grub-mkconfig -o /boot/grub/grub.cfg
 "
 
 # --------------------------------------------------------------------------------
