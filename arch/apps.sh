a2be89fb66c70b95bffc0f9d0f1a1d17ea6ddca3
Alexander Elias <alex.steven.elias@gmail.com>
2019-10-10 00:32:29 -0700

    added font

--- arch/apps.sh
+++ arch/apps.sh
@@ -5,6 +5,6 @@ steam aws-cli \
 inkscape gimp \
 chromium firefox \
 atom nodejs npm vim \
-ttf-fira-code ttf-fira-mono ttf-fira-sans
+ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-dejavu
 
 yay -S spotify enpass-bin brave-bin hyper-bin
