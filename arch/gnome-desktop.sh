d6d74a6511f68045fa5d226b28ada89a87db982b
Alexander Elias <alex.steven.elias@gmail.com>
2019-10-13 14:25:18 -0700

    added seahorse

--- arch/gnome-desktop.sh
+++ arch/gnome-desktop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pacman -S gnome gnome-tweaks chrome-gnome-shell
+pacman -S gnome gnome-tweaks chrome-gnome-shell seahorse
 
 systemctl enable gdm.service
 systemctl start gdm.service
